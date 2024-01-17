773118052c3b9f32c1e7f8c384f61335dc25b899
AMSemse <112924939+AMSemse@users.noreply.github.com>
2024-01-17 04:31:24 +0200

    exercise

--- project2_2Dplatformer/GameManager.cs
+++ project2_2Dplatformer/GameManager.cs
@@ -1,13 +1,21 @@
 using Godot;
 using System;
+using System.Drawing;
 
 public partial class GameManager : Node
 {
 	int points = 0;
+	private PointsLabel Label;
+	public override void _Ready()
+	{
+		Label = GetNode<PointsLabel>("../UI/Panel/PointsLabel");
+		Label.Text = points.ToString() + "/6";
+	}
 
 	public void AddPoints()
 	{
 		points++;
-		GD.Print(points);
+		//GD.Print(points);
+		Label.Text = points.ToString() + "/6";
 	}
 }
--- project2_2Dplatformer/PointsLabel.cs
+++ project2_2Dplatformer/PointsLabel.cs
@@ -1,26 +1,15 @@
 using Godot;
 using System;
 
-
-public partial class Collectible : Area2D
+public partial class PointsLabel : Label
 {
-
-	private GameManager Node;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		Node = GetNode<GameManager>("../../GameManager");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
 	}
-	private void OnCollectibleBodyEntered(CharacterBody2D body)
-	{
-		QueueFree();
-		Node.AddPoints();
-	}
-
 }
-
